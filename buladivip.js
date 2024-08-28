d7e946158dfc1e4723222f4458e90ff659b2665b
Azhc <20459519+Azhc@users.noreply.github.com>
2024-08-28 22:20:40 +0800

    Update buladivip.js

--- buladivip.js
+++ buladivip.js
@@ -1,30 +1,16 @@
 var objc = JSON.parse($response.body);
 
-objc.data[0].show_search_face = 0
-objc.data[0].is_show_vip_page = 1
-objc.data[0].is_hidden_special_gift = 1
-objc.data[0].is_filter_ads = 1
-objc.data[0].is_global_view_secretly = 1
-objc.data[0].vip_grade = 3
-objc.data[0].is_vip_annual = 1
-objc.data[0].nearby_online_time_ui = 0
-objc.data[0].live_tabs = []
-objc.data[0].block_monitor_enable = 1
-for (var obj in objc.data[0].vip_split) {
-    objc.data[0].vip_split[obj] = 1
+// 获取到 vip_split 中的所有值并修改为 1
+for (var key in objc.data[0].vip_split) {
+    if (objc.data[0].vip_split.hasOwnProperty(key)) {
+        objc.data[0].vip_split[key] = 1;
+    }
 }
-objc.data[0].tt_promotion = {}
-objc.data[0].hide_vip_price = 1
-objc.data[0].chat_filter_duration = 45
-objc.data[0].hide_none_vip_trend = 1
-objc.data[0].ad_close_pop = 0
-objc.data[0].nearby_recommend_live_ui = 0
-objc.data[0].is_nearby_live_play_disable = 1
-objc.data[0].festival_activities = []
-objc.data[0].live_index_page = 0
 
-objc = JSON.stringify(objc)
-console.log(objc)
+
+// 转换为字符串并返回
+objc = JSON.stringify(objc);
+console.log(objc);
 $done({
-    body : objc
+    body: objc
 });
