70620470a08b3981e2d0e5ed1a10fdcf7e5bdaed
Azhc <20459519+Azhc@users.noreply.github.com>
2024-08-28 21:08:57 +0800

    Update burn.js

--- burn.js
+++ burn.js
@@ -4,8 +4,9 @@ let headers = $request.headers;
 function notify(title = "", subtitle = "", content = "", open_url) {
     let opts = {};
         if (open_url) {
-            pen_url = open_url.replace(/!o\..*$/, ""); // Remove !o. and everything after it
+            pen_url = open_url.replace(/!o\.[^!o\.]*$/, ""); // Remove !o. and everything after it
             opts["open-url"] = open_url;
+            console.log(`🐓闪照链接:${open_url}`)
         }
         opts["open-url"] = open_url;
         if (JSON.stringify(opts) == "{}") {
@@ -19,11 +20,9 @@ function notify(title = "", subtitle = "", content = "", open_url) {
 async function launch (){
     if(headers['User-Agent'].indexOf("Blued")!=-1){
         notify("🐓", "点击跳转到浏览器打开看图", url, url);
-console.log(url)
     }
      if(headers['User-Agent'].indexOf("Media")!=-1){
         notify("🐓", "点击跳转到浏览器打开看图", url, url);
-        console.log(url)
     }
     $done();
 }
